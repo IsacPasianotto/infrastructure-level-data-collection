e5ca93846dcd45d03a5a698f140204d3a907f423
Isac Pasianotto <yh4mop12z@gmail.com>
2026-04-20 11:47:58 +0200

    instruction on telegraf container building

---
+++ 01-telegraf-deployment/README.md
@@ -0,0 +1,29 @@
+# telegraf-deployment
+
+
+## Bulid the container:
+
+```bash
+export DOCKERHUB_USERNAME=<your_username>
+./build_container.sh
+```
+
+## How to deploy everything:
+
+> Reporducibility disclaimer: In the `templates/telegraf-config.toml.j2` file, you may need to adjust the db url accordingly to your setup. 
+> Obviously, you also need to adjust the inventory file to match your setup.
+```
+ansible-playbook --key-file=$HOME/.ssh/<your_key> install.yaml
+```
+
+And then deploy the content of the `server-wide-metrics` directory with
+
+### Manual install
+
+- build the container
+- populate the /etc/containers/systemd/<file>.container
+- `systemctl daemon-reload`
+- `systemctl start <file>.service`
+
+Debug the generated unit with `/usr/libexec/podman/quadlet --dryrun <file>.container`
+
---
+++ 01-telegraf-deployment/build_container.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+export PROJECT_DIR=$(git rev-parse --show-toplevel)
+pushd $PROJECT_DIR > /dev/null || exit 1
+
+echo "============ building the container ============ "
+
+pushd ./container-image > /dev/null || exit 1
+
+# clone the repo and appl the custom config
+if [ ! -d "telegraf" ]; then
+    git clone -b v1.37.0 https://github.com/influxdata/telegraf.git
+fi
+
+pushd telegraf > /dev/null || exit 1
+# git apply amd.patch
+#  ----- PATCH ----
+# before:
+#      case "package", "node", "die", "core", "cpu", "apic", "x2apic":
+# after:
+#      case "package", "node", "die", "core", "cpu", "apic", "x2apic", "l3":
+#
+sed -i '/"l3"/! s/"x2apic":/"x2apic", "l3":/' plugins/inputs/turbostat/turbostat.go
+# before:
+#     	case "v12":
+# after:
+#       case "v12", "v13":
+sed -i '/"v13"/! s/"v12":/"v12", "v13":/' plugins/inputs/nvidia_smi/nvidia_smi.go
+
+
+
+echo " ----- building the telegraf patched binaries ----- "
+podman run --memory=8g --pids-limit=-1 --rm -v $(pwd):/usr/src/telegraf -w /usr/src/telegraf golang:1.25 make
+echo " ----- done ----- "
+
+# exit telegraf dir
+popd > /dev/null || exit 1
+
+
+podman build -t telegraf-monitoring .
+podman tag telegraf-monitoring docker.io/${DOCKERHUB_USERNAME:-$(whoami)}/telegraf-monitoring:latest
+podman login docker.io
+
+# exit the container-image dir
+popd > /dev/null || exit 1
+echo "=========== container built successfully =========="
+echo "=========== pushing container images to docker.io =========="
+podman push docker.io/${DOCKERHUB_USERNAME:-$(whoami)}/telegraf-monitoring:latest
+
+# return in the original dir
+popd > /dev/null || exit 1
